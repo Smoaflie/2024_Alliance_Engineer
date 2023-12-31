c1d43cc6cf02450dfd0f9f5eb102dc19903fc5da
Smoaflie <handsome_shit@outlook.com>
2023-12-31 10:22:50 +0800

    写了个Pitch限位
    Pitch限位

--- application/robot_def.h
+++ application/robot_def.h
@@ -26,11 +26,13 @@
 
 /* 机器人重要参数定义,注意根据不同机器人进行修改,浮点数需要以.0或f结尾,无符号以u结尾 */
 // 云台参数
-#define YAW_CHASSIS_ALIGN_ECD     2711 // 云台和底盘对齐指向相同方向时的电机编码器值,若对云台有机械改动需要修改
+#define YAW_CHASSIS_ALIGN_ECD     1356 // 云台和底盘对齐指向相同方向时的电机编码器值,若对云台有机械改动需要修改
 #define YAW_ECD_GREATER_THAN_4096 0    // ALIGN_ECD值是否大于4096,是为1,否为0;用于计算云台偏转角度
-#define PITCH_HORIZON_ECD         3412 // 云台处于水平位置时编码器值,若对云台有机械改动需要修改
-#define PITCH_MAX_ANGLE           0    // 云台竖直方向最大角度 (注意反馈如果是陀螺仪，则填写陀螺仪的角度)
-#define PITCH_MIN_ANGLE           0    // 云台竖直方向最小角度 (注意反馈如果是陀螺仪，则填写陀螺仪的角度)
+#define PITCH_HORIZON_ECD         4073 // 云台处于水平位置时编码器值,若对云台有机械改动需要修改
+#define PITCH_POS_UP_LIMIT_ECD    3670 // 云台竖直方向高处限位编码器值,若对云台有机械改动需要修改
+#define PITCH_POS_DOWN_LIMIT_ECD  4455 // 云台竖直方向低处限位编码器值,若对云台有机械改动需要修改
+#define PITCH_FEED_TYPE           1    // 云台PITCH轴反馈值来源:编码器为0,陀螺仪为1
+#define PITCH_ECD_UP_ADD          0    // 云台抬升时编码器变化趋势,增为1,减为0 (陀螺仪变化方向应相同)
 // 发射参数
 #define ONE_BULLET_DELTA_ANGLE 36    // 发射一发弹丸拨盘转动的距离,由机械设计图纸给出
 #define REDUCTION_RATIO_LOADER 49.0f // 拨盘电机的减速比,英雄需要修改为3508的19.0f
