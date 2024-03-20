1e6ea56abe96dafd496160ccc882a6b9596f6cae
Smoaflie <handsome_shit@outlook.com>
2024-03-20 15:23:04 +0800

    新增编码器支持型号 | 新增编码器速度测量值信息（未测试) | 新增测试模式

--- application/robot_task.h
+++ application/robot_task.h
@@ -16,107 +16,7 @@
 #include "buzzer.h"
 
 #include "bsp_log.h"
-//! 任务直接在cubeMX中配置,不再使用这种方式
-#if 0
-osThreadId insTaskHandle;
-osThreadId robotTaskHandle;
-osThreadId motorTaskHandle;
-osThreadId daemonTaskHandle;
-osThreadId uiTaskHandle;
 
-void StartINSTASK(void const *argument);
-void StartMOTORTASK(void const *argument);
-void StartDAEMONTASK(void const *argument);
-void StartROBOTTASK(void const *argument);
-void StartUITASK(void const *argument);
-
-/**
- * @brief 初始化机器人任务,所有持续运行的任务都在这里初始化
- *
- */
-void OSTaskInit()
-{
-        osThreadDef(instask, StartINSTASK, osPriorityAboveNormal, 0, 1024);
-        insTaskHandle = osThreadCreate(osThread(instask), NULL); // 由于是阻塞读取传感器,为姿态解算设置较高优先级,确保以1khz的频率执行
-        // 后续修改为读取传感器数据准备好的中断处理,
-
-        osThreadDef(motortask, StartMOTORTASK, osPriorityNormal, 0, 256);
-        motorTaskHandle = osThreadCreate(osThread(motortask), NULL);
-
-        osThreadDef(daemontask, StartDAEMONTASK, osPriorityNormal, 0, 128);
-        daemonTaskHandle = osThreadCreate(osThread(daemontask), NULL);
-
-        osThreadDef(robottask, StartROBOTTASK, osPriorityNormal, 0, 1024);
-        robotTaskHandle = osThreadCreate(osThread(robottask), NULL);
-
-        osThreadDef(uitask, StartUITASK, osPriorityNormal, 0, 512);
-        uiTaskHandle = osThreadCreate(osThread(uitask), NULL);
-
-        HTMotorControlInit(); // 没有注册HT电机则不会执行
-}
-
-
-__attribute__((noreturn)) void StartMOTORTASK(void const *argument)
-{
-    static float motor_dt;
-    static float motor_start;
-    LOGINFO("[freeRTOS] MOTOR Task Start");
-    for (;;) {
-        motor_start = DWT_GetTimeline_ms();
-        MotorControlTask();
-        motor_dt = DWT_GetTimeline_ms() - motor_start;
-        if (motor_dt > 1)
-            LOGERROR("[freeRTOS] MOTOR Task is being DELAY! dt = [%f]", &motor_dt);
-        osDelay(1);
-    }
-}
-
-__attribute__((noreturn)) void StartDAEMONTASK(void const *argument)
-{
-    static float daemon_dt;
-    static float daemon_start;
-    BuzzerInit();
-    LOGINFO("[freeRTOS] Daemon Task Start");
-    for (;;) {
-        // 100Hz
-        daemon_start = DWT_GetTimeline_ms();
-        DaemonTask();
-        BuzzerTask();
-        daemon_dt = DWT_GetTimeline_ms() - daemon_start;
-        if (daemon_dt > 10)
-            LOGERROR("[freeRTOS] Daemon Task is being DELAY! dt = [%f]", &daemon_dt);
-        osDelay(10);
-    }
-}
-
-__attribute__((noreturn)) void StartROBOTTASK(void const *argument)
-{
-    static float robot_dt;
-    static float robot_start;
-    LOGINFO("[freeRTOS] ROBOT core Task Start");
-    // 200Hz-500Hz,若有额外的控制任务如平衡步兵可能需要提升至1kHz
-    for (;;) {
-        robot_start = DWT_GetTimeline_ms();
-        RobotTask();
-        robot_dt = DWT_GetTimeline_ms() - robot_start;
-        if (robot_dt > 5)
-            LOGERROR("[freeRTOS] ROBOT core Task is being DELAY! dt = [%f]", &robot_dt);
-        osDelay(5);
-    }
-}
-
-__attribute__((noreturn)) void StartUITASK(void const *argument)
-{
-    LOGINFO("[freeRTOS] UI Task Start");
-    MyUIInit();
-    LOGINFO("[freeRTOS] UI Init Done, communication with ref has established");
-    for (;;) {
-        // 每给裁判系统发送一包数据会挂起一次,详见UITask函数的refereeSend()
-        UITask();
-        osDelay(1); // 即使没有任何UI需要刷新,也挂起一次,防止卡在UITask中无法切换
-    }
-}
-#endif
 #include "led.h"
 #include "buzzer.h"
 #include "ins_task.h"
@@ -138,8 +38,6 @@ __attribute__((noreturn)) void StartINSTASK(void *argument)
     }
 }
 
-extern void MotorControlTask();
-
 __attribute__((noreturn)) void TestTask(void *argument)
 {
     UNUSED(argument);
@@ -147,10 +45,7 @@ __attribute__((noreturn)) void TestTask(void *argument)
     BuzzerPlay(StartUP_sound);
 
     while (1) {
-        C_board_LEDSet(0x33ffff);
-        /*osDelay(500);
-        C_board_LEDSet(0xd633ff);
-        osDelay(500);*/
+        RobotTask();
         MotorControlTask();
 
         osDelay(1);
--- modules/encoder/encoder.h
+++ modules/encoder/encoder.h
@@ -15,13 +15,21 @@
 
 #define ENCODER_CNT 5
 
-#define ECD_ANGLE_TO_DEG 0.0013733f // (360/262,144),将编码器值转化为角度制
+#define MT6825_ECD_TO_DEG 0.0013733f // (360/262,144),将编码器值转化为角度制
+#define me02_ECD_TO_DEG 0.01098633f // (360/32768)
+/* 编码器类型枚举 */
+typedef enum
+{
+    MT6825 = 0,
+    me02_can,
+} Encoder_Type_e;
 
 typedef struct
 {
     uint32_t last_ecd;        // 上一次读取的编码器值
     uint32_t ecd;             // 0-65535,刻度总共有65535格
     float angle_single_round; // 单圈角度
+    float speed_aps;    // 旋转速度,度每秒
 
     float total_angle;   // 总角度,注意方向
     int32_t total_round; // 总圈数,注意方向
@@ -32,22 +40,25 @@ typedef struct
 {
     CAN_Init_Config_s can_init_config;
 
-    uint32_t offset;    //电机偏移量|可选
+    Encoder_Type_e encoder_type; // 编码器类型
+    uint32_t offset;    //编码器偏移量|可选
 } Encoder_Init_Config_s;
 
 typedef struct
 {
-    Encoder_Measure_s measure;            // 电机测量值
+    Encoder_Measure_s measure;            // 编码器测量值
 
-    uint32_t offset;    //电机偏移量|可选
+    uint32_t offset;    //编码器偏移量|可选
 
-    CANInstance *encoder_can_instance; // 电机CAN实例
+    Encoder_Type_e encoder_type; // 编码器类型
+    CANInstance *encoder_can_instance; // 编码器CAN实例
 
     DaemonInstance* daemon;
 
     uint32_t feed_cnt;
     float dt;
 } EncoderInstance_s;
+
 
 // 编码器初始化,返回一个编码器实例
 EncoderInstance_s *EncoderInit(Encoder_Init_Config_s *config);
