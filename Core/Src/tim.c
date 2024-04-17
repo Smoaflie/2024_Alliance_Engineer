5bb722be4fc9e70f9be80392a4d5cc3f4ec3ce17
Smoaflie <handsome_shit@outlook.com>
2024-04-17 15:59:23 +0800

    修改TIM分频参数，使能PWM输出，默认开启5V输出

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -45,7 +45,7 @@ void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 275-1;
+  htim1.Init.Prescaler = 240-1;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim1.Init.Period = 19999;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
@@ -113,7 +113,7 @@ void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 275-1;
+  htim2.Init.Prescaler = 240-1;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim2.Init.Period = 19999;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
@@ -161,7 +161,7 @@ void MX_TIM3_Init(void)
 
   /* USER CODE END TIM3_Init 1 */
   htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 275-1;
+  htim3.Init.Prescaler = 240-1;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim3.Init.Period = 10000-1;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
@@ -205,7 +205,7 @@ void MX_TIM12_Init(void)
 
   /* USER CODE END TIM12_Init 1 */
   htim12.Instance = TIM12;
-  htim12.Init.Prescaler = 275-1;
+  htim12.Init.Prescaler = 240-1;
   htim12.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim12.Init.Period = 1000-1;
   htim12.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
--- application/test/test.c
+++ application/test/test.c
@@ -13,16 +13,33 @@
 #include "encoder.h"
 #include "bsp_dwt.h"
 #include "bsp_log.h"
+#include "servo_motor.h"
+
+static ServoInstance *leftservomoto;
 
 void selfTestInit()
 {
-
+    //初始化参数
+    Servo_Init_Config_s config={
+        //舵机安装选择的定时器及通道
+        //C板有常用的7路PWM输出:TIM1-1,2,3,4 TIM8-1,2,3
+        .htim=&htim2,
+        .Channel=TIM_CHANNEL_1,
+        //舵机的初始化模式和类型
+        .Servo_Angle_Type=Start_mode,
+        .Servo_type=Servo180,
+    };
+    // 设置好参数后进行初始化并保留返回的指针
+    leftservomoto = ServoInit(&config);
 }
 
+int32_t angle = 0;
 /* 机器人机械臂控制核心任务 */
 void selfTestTask()
 {
+    Servo_Motor_FreeAngle_Set(leftservomoto,angle);
     DM_board_LEDSet(0x000000);
+    ServeoMotorControl();
     osDelay(1000);
     DM_board_LEDSet(0xff0000);
     osDelay(1000);
